5c0f71d82f39927344d79c8fba8985eddf9f75b4
Daniel Estiller <estillerdaniel@gmail.com>
2024-02-21 14:53:55 +0800

    feat: Add seeder for admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,13 +10,8 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
-    public function run(): void
+    public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call(UserSeeder::class);
     }
 }
